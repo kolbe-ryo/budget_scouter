a5d8e276d759fe1fbdf828572cac881cfc01fdcc
kolbe <water.northground@gmail.com>
2022-07-10 16:18:59 +0900

    add necessary page

---
+++ lib/view/page/money_meter_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MoneyMeterPage extends StatelessWidget {
+  const MoneyMeterPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
